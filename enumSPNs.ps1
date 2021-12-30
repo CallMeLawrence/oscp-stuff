7f18a34c32871a81eac08c4d905de6aa56dde08f
Edward <edwardlpluth@gmail.com>
2021-12-29 18:27:32 -0600

    Added
    Added files I found in the nether.

---
+++ enumSPNs.ps1
@@ -0,0 +1,18 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$PDC = ($domainObj.PdcRoleOwner).Name
+$SearchString = "LDAP://"
+$SearchString += $PDC + "/"
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+$SearchString += $DistinguishedName
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+$Searcher.SearchRoot = $objDomain
+$Searcher.filter="serviceprincipalname=*http*"
+$Result = $Searcher.FindAll()
+ForEach($obj in $Result)
+{
+	ForEach($prop in $obj.Properties)
+	{
+		$prop
+	}
+}
---
+++ enumerateAD.ps1
@@ -0,0 +1,32 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+$PDC = ($domainObj.PdcRoleOwner).Name
+
+$SearchString = "LDAP://"
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+
+$SearchString += $DistinguishedName
+
+# $SearchString
+
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+
+$Searcher.SearchRoot = $objDomain
+
+$Searcher.filter="samAccountType=805306368" ## Change ME ##
+
+$Searcher.FindAll()
+
+Foreach($obj in $Result)
+{
+	ForEach($prop in $obj.Properties)
+	{
+	    $prop
+	}
+	Write-Host "--------------------"
+}
